c5731671ee2011a37f276d1fd6df316256695b86
Miguel Ángel Padilla Bernabé <57225564+miguelpb97@users.noreply.github.com>
2025-10-07 01:18:58 +0200

    otros cambios

--- Java Utilities/app/src/main/java/com/mapb/javautilities/utilities/ValidarDni.java
+++ Java Utilities/app/src/main/java/com/mapb/javautilities/utilities/ValidarDni.java
@@ -8,17 +8,23 @@ import java.util.regex.Pattern;
  * @author MAPB
  */
 public class ValidarDni {
-	private static final String ER_DNI = "(\\d{8})([A-Za-z]{1})";
+	private static final String ER_DNICOMPLETO = "(\\d{8})([A-Za-z]{1})";
+	private static final String ER_DNISINLETRA = "(\\d{8})";
 	private static char letras[] = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H',
 			'L', 'C', 'K', 'E' };
 
+    /**
+     * Constructor privado para evitar que java genere el suyo por defecto y así
+     * evitar que se puedan instanciar objetos
+     */
 	private ValidarDni() {
 		// No se permiten instanciar objetos de esta clase
 	}
 
+	// Validar dni pasado pro parametro
 	public static boolean validarDni(String dni) {
 		boolean comprobarDni = false;
-		Pattern p = Pattern.compile(ER_DNI);
+		Pattern p = Pattern.compile(ER_DNICOMPLETO);
 		Matcher m = p.matcher(dni);
 		String numeros = "";
 		String letra = "";
@@ -35,6 +41,19 @@ public class ValidarDni {
 			comprobarDni = true;
 		}
 		return comprobarDni;
+	}
+	
+	// Obtener la letra correspondiente a un dni dado por parametro
+	public static char obtenerLetraDni(String dni) {
+		Pattern p = Pattern.compile(ER_DNISINLETRA);
+		Matcher m = p.matcher(dni);
+		String numeros = "";
+		while (m.matches()) {
+			numeros = m.group(1);
+		}
+		int dniInt = Integer.parseInt(numeros);
+		int resto = dniInt % 23;
+		return letras[resto];
 	}
 
 }
